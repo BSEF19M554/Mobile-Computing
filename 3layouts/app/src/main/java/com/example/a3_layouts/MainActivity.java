d5a7a0ac8e6977199e2dc30bd94d3b5112b0d763
Ahmed Mubarak Faisal <bsef19m554@pucit.edu.pk>
2022-04-25 01:10:37 +0500

    First commit

---
+++ 3layouts/app/src/main/java/com/example/a3_layouts/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a3_layouts;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button changeTextButton;
+    TextView textToChange;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textToChange = findViewById(R.id.textView6);
+        changeTextButton = findViewById(R.id.button14);
+        changeTextButton.setOnClickListener(view -> textToChange.setText(R.string.onClickEvent));
+    }
+}
