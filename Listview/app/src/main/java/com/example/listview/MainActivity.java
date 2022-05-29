738029cb1c7efc28a71a3b817e5c71773e044ea6
Ahmed Mubarak Faisal <bsef19m554@pucit.edu.pk>
2022-05-29 21:02:29 +0500

    Added a new project and implemented basic listview

---
+++ Listview/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.listview;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myList;
+    String[] myString = {"Lahore", "Islamabad", "Faisalabad"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myList = findViewById(R.id.list);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, myString);
+        myList.setAdapter(myAdapter);
+
+        myList.setOnItemClickListener((adapterView, view, i, l) -> Toast.makeText(MainActivity.this, "Clicked" + myString[i], Toast.LENGTH_SHORT).show());
+    }
+}
